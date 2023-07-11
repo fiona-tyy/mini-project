2f727adc41eedaf1db5505df38e69c88b63ed608
fiona-tyy <zyyfiona@hotmail.com>
2023-07-11 11:58:06 +0800

    added navigator.share

--- app-frontend/src/app/components/expense-summary.component.ts
+++ app-frontend/src/app/components/expense-summary.component.ts
@@ -4,6 +4,7 @@ import { ExpenseProcessed, SettlementData, Transaction, User } from '../model';
 import { Observable, Subscription, firstValueFrom } from 'rxjs';
 import { UserService } from '../services/user.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-expense-summary',
@@ -16,6 +17,7 @@ export class ExpenseSummaryComponent implements OnInit {
   summary$!: Observable<Transaction[]>;
   // payer!: User;
   url!: string;
+  shareSupported?: boolean;
 
   constructor(
     private expenseSvc: ExpenseService,
@@ -31,11 +33,37 @@ export class ExpenseSummaryComponent implements OnInit {
     this.url = 'https://fiona-tyy.com/images/' + transactionId;
     // post through service
     this.summary$ = this.expenseSvc.getTransactionById(transactionId);
+    this.shareSupported = !!navigator.canShare;
   }
 
   delete(transactionId: string) {
     firstValueFrom(this.expenseSvc.deleteTransactionById(transactionId))
-      .then((result) => this.router.navigate(['/home']))
+      .then(() => this.router.navigate(['/home']))
       .catch((err) => alert(err.message));
   }
+
+  share() {
+    let node = document.getElementById('page-summary');
+    htmlToImage
+      .toPng(node!)
+      .then((dataUrl) => {
+        console.info('dataUrl', dataUrl);
+        return dataUrlToFile(dataUrl, 'screenshot.png');
+      })
+      .then((file) => {
+        navigator.share({
+          text: "[NinjaSplit] Here's how much you owe:",
+          files: [file],
+        });
+      });
+  }
+}
+
+export async function dataUrlToFile(
+  dataUrl: string,
+  fileName: string
+): Promise<File> {
+  const res: Response = await fetch(dataUrl);
+  const blob: Blob = await res.blob();
+  return new File([blob], fileName, { type: blob.type });
 }
